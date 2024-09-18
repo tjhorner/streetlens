ea11eb5cf90e11289e471f9eb03d013282111d74
TJ Horner <me@tjhorner.com>
2024-09-18 16:16:44 -0700

    frick

--- packages/frontend/src/routes/+page.svelte
+++ packages/frontend/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
-  import "../../app.css"
+  import "../app.css"
   import TrackExplorer from "$lib/components/TrackExplorer.svelte"
+  import Notifier from "$lib/components/Notifier.svelte"
 </script>
 
+<Notifier />
 <TrackExplorer />
